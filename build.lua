4a37a94631931d0306128903ce57010c2ea50d27
David Cruz <david@codebycruz.com>
2026-04-11 20:56:29 -0700

    build: try getting android working again..

--- build.lua
+++ build.lua
@@ -1,7 +1,9 @@
 local outDir = os.getenv("LDE_OUTPUT_DIR")
 local sep = string.sub(package.config, 1, 1)
-local isWindows = sep == "\\"
-local isMac = not isWindows and io.popen("uname"):read("*l") == "Darwin"
+local isWindows = jit.os == "Windows"
+local isMac = jit.os == "OSX"
+local isAndroid = os.getenv("ANDROID_ROOT") ~= nil
+local sh = isAndroid and "/data/data/com.termux/files/usr/bin/sh" or "/bin/sh"
 local scriptDir = debug.getinfo(1, "S").source:sub(2):match("(.*[/\\])")
 local curlSrc = scriptDir .. "vendor" .. sep .. "curl"
 local libName = isWindows and "curl.dll" or isMac and "libcurl.dylib" or "libcurl.so"
@@ -11,28 +13,29 @@ local outLib = outDir .. sep .. libName
 if io.open(outLib, "rb") then return end
 
 local function exec(cmd)
-    local ret = os.execute(cmd)
-    assert(ret == 0 or ret == true, "command failed: " .. cmd)
+	local ret = os.execute(cmd)
+	assert(ret == 0 or ret == true, "command failed: " .. cmd)
 end
 
+---@format disable-next
 if isWindows then
-    exec('cmake -S "' .. curlSrc .. '" -B "' .. curlSrc .. '\\build" -DBUILD_SHARED_LIBS=ON -DBUILD_CURL_EXE=OFF -DCURL_USE_LIBPSL=OFF -DCURL_USE_SCHANNEL=ON')
-    exec('cmake --build "' .. curlSrc .. '\\build" --config Release')
-    exec('copy "' .. curlSrc .. '\\build\\lib\\Release\\libcurl.dll" "' .. outLib .. '"')
+	exec('cmake -S "' .. curlSrc .. '" -B "' .. curlSrc .. '\\build" -DBUILD_SHARED_LIBS=ON -DBUILD_CURL_EXE=OFF -DCURL_USE_LIBPSL=OFF -DCURL_USE_SCHANNEL=ON')
+	exec('cmake --build "' .. curlSrc .. '\\build" --config Release')
+	exec('copy "' .. curlSrc .. '\\build\\lib\\Release\\libcurl.dll" "' .. outLib .. '"')
 else
-    exec('cd "' .. curlSrc .. '" && autoreconf -fi && ./configure --disable-static --enable-shared --with-openssl --without-libpsl && make -j$(nproc)')
-    local builtLib = isMac and (curlSrc .. "/lib/.libs/libcurl.dylib") or (curlSrc .. "/lib/.libs/libcurl.so")
-    exec('cp "' .. builtLib .. '" "' .. outLib .. '"')
+	exec('cd "' .. curlSrc .. '" && SHELL="' .. sh .. '" autoreconf -fi && CONFIG_SHELL="' .. sh .. '" ./configure --disable-static --enable-shared --with-openssl --without-libpsl && make -j$(nproc)')
+	local builtLib = isMac and (curlSrc .. "/lib/.libs/libcurl.dylib") or (curlSrc .. "/lib/.libs/libcurl.so")
+	exec('cp "' .. builtLib .. '" "' .. outLib .. '"')
 
-    -- generate embedded CA bundle as a Lua file
-    local cacertLua = outDir .. "/cacert.lua"
-    if not io.open(cacertLua, "rb") then
-        local tmp = outDir .. "/cacert.pem"
-        exec('curl -sSL https://curl.se/ca/cacert.pem -o "' .. tmp .. '"')
-        local pem = assert(io.open(tmp, "rb")):read("*a")
-        local f = assert(io.open(cacertLua, "wb"))
-        f:write("return [=[\n" .. pem .. "]=]\n")
-        f:close()
-        os.remove(tmp)
-    end
+	-- generate embedded CA bundle as a Lua file
+	local cacertLua = outDir .. "/cacert.lua"
+	if not io.open(cacertLua, "rb") then
+		local tmp = outDir .. "/cacert.pem"
+		exec('curl -sSL https://curl.se/ca/cacert.pem -o "' .. tmp .. '"')
+		local pem = assert(io.open(tmp, "rb")):read("*a")
+		local f = assert(io.open(cacertLua, "wb"))
+		f:write("return [=[\n" .. pem .. "]=]\n")
+		f:close()
+		os.remove(tmp)
+	end
 end
